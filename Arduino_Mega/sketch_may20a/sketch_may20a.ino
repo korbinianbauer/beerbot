a8ead9e29505726f25d068a84d9f85a0da18e452
couka <spams3141592@web.de>
2020-05-22 13:10:15 +0200

    jhbhjb

---
+++ Arduino_Mega/sketch_may20a/sketch_may20a.ino
@@ -0,0 +1,45 @@
+// PWM - Testprogramm
+//Code fuer Arduino
+//Author Retian
+//Version 1.0
+
+#define potiPin 2 //Eingang Poti ist A2
+
+int potiWert;
+
+void setup() {
+  Serial.begin(115200);
+  //Löschen der Timer/Counter Control Register A und B
+
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  //Modus Fast PWM-Mode 10 Bit einstellen
+  TCCR1A |= (1 << WGM10) | (1 << WGM11);
+  TCCR1B |= (1 << WGM12);
+
+  //Vorteiler auf 1 setzen
+  TCCR1B |= (1 << CS10);
+
+  //Nichtinvertiertes PWM-Signal setzen
+  TCCR1A |= (1 << COM1A1);
+
+  //PWM-Pin 9 als Ausgang definieren
+  DDRB |= (1 << DDB1);
+}
+
+
+
+void loop() {
+  potiWert = 250; //Potiwert einlesen (Auflösung Analogeingang = 10 Bit)
+
+  //Ist die Auflösung des PWM-Signals z.B 8 Bit, muss der potiWert angepasst werden:
+  //potiWert = map(potiWert, 0, 1023, 0, 255);
+
+  OCR1A = potiWert; // Setzen des Impuls-Pausenverhältnis
+
+  Serial.println(potiWert);
+
+  delay(15);
+}
+
---
+++ Arduino_Mega/sketch_may22a/sketch_may22a.ino
@@ -0,0 +1,125 @@
+#include <PWM.h>
+
+#define M_LEFT_FORWARD 3
+#define M_LEFT_BACKWARD 2
+#define M_LEFT_FWD_EN 5
+#define M_LEFT_BWD_EN 4
+
+#define M_LEFT_POS A0
+
+#define M_RIGHT_FORWARD 7
+#define M_RIGHT_BACKWARD 8
+#define M_RIGHT_FWD_EN 9
+#define M_RIGHT_BWD_EN 6
+
+
+
+class Motor {
+  public:
+    int pin_fwd_en;
+    int pin_bwd_en;
+    int pin_fwd;
+    int pin_bwd;
+
+    Motor(int fwd_en, int bwd_en, int fwd, int bwd) {
+      InitTimersSafe();
+      pin_fwd_en = fwd_en;
+      pin_bwd_en = bwd_en;
+      pin_fwd = fwd;
+      pin_bwd = bwd;
+    }
+
+
+
+    void setPinPWM(int pin, int value) {
+      // pin: Physical Arduino Pin Number
+      // value: 0...100
+      pinMode(pin, OUTPUT);
+
+      int pwm_pseudo = 0;
+      switch (pin) {
+        case 2: pwm_pseudo = 5; break;
+        case 3: pwm_pseudo = 2; break;
+        case 7: pwm_pseudo = 6; break;
+        case 8: pwm_pseudo = 7; break;
+        case 11: pwm_pseudo = 11; break;
+        case 12: pwm_pseudo = 12; break;
+        case 45: pwm_pseudo = 46; break;
+        case 44: pwm_pseudo = 45; break;
+      }
+      SetPinFrequency(pwm_pseudo, 20000);
+      long pwm_value = value * 655;
+      pwmWriteHR(pwm_pseudo, pwm_value);
+
+    }
+
+    void set_power(int value) {
+      pinMode(pin_fwd, OUTPUT);
+      pinMode(pin_bwd, OUTPUT);
+      pinMode(pin_fwd_en, OUTPUT);
+      pinMode(pin_bwd_en, OUTPUT);
+
+      digitalWrite(pin_fwd_en, HIGH);
+      digitalWrite(pin_bwd_en, HIGH);
+
+      if (value > 0) {
+        // Low PWM values are not transfered due to MosFET Rise- and Fall time, so we have to give it a little extra
+        value = map(value, 1, 100, 16, 100);
+        setPinPWM(pin_fwd, value);
+        digitalWrite(pin_bwd, LOW);
+      } else if (value < 0) {
+        // Low PWM values are not transfered due to MosFET Rise- and Fall time, so we have to give it a little extra
+        value = map(value, -1, -100, 16, 100);
+        setPinPWM(pin_bwd, value);
+        digitalWrite(pin_fwd, LOW);
+      } else {
+        digitalWrite(pin_fwd, LOW);
+        digitalWrite(pin_bwd, LOW);
+      }
+    }
+
+    void set_brake(int value) {
+      pinMode(pin_fwd, OUTPUT);
+      pinMode(pin_bwd, OUTPUT);
+      pinMode(pin_fwd_en, OUTPUT);
+      pinMode(pin_bwd_en, OUTPUT);
+
+      digitalWrite(pin_fwd_en, HIGH);
+      digitalWrite(pin_bwd_en, HIGH);
+      setPinPWM(pin_fwd, value);
+      setPinPWM(pin_bwd, value);
+    }
+
+};
+
+
+void setup() {
+  Serial.begin(250000);
+  Motor MotorLeft = Motor(M_LEFT_FWD_EN, M_LEFT_BWD_EN, M_LEFT_FORWARD, M_LEFT_BACKWARD);
+  Motor MotorRight = Motor(M_RIGHT_FWD_EN, M_RIGHT_BWD_EN, M_RIGHT_FORWARD, M_RIGHT_BACKWARD);
+
+  delay(5000);
+  MotorLeft.set_power(20);
+  delay(2000);
+  MotorLeft.set_brake(100);
+  delay(100);
+  MotorLeft.set_brake(0);
+
+
+}
+
+void loop() {
+  // Wait for zero position
+  while (analogRead(M_LEFT_POS) != 0) {}
+  while (analogRead(M_LEFT_POS) == 0) {}
+  Serial.println(0);
+  int pos = analogRead(M_LEFT_POS);
+  while (pos != 1023) {
+    Serial.println(pos);
+    delayMicroseconds(2000);
+    pos = analogRead(M_LEFT_POS);
+  }
+  delay(100000L);
+
+
+}
